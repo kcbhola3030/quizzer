828b6dbfc94282e58de42c9c53e0c7f829391e96
kc <kcbhola123@gmail.com>
2023-01-26 11:46:14 +0530

    Database connection n all added

--- API/index.js
+++ API/index.js
@@ -1,72 +1,98 @@
-const  importData = require("./data.json");
+var importData = [
+    {
+        "id":"1",
+        "question":"Who is not the creator Golang?",
+        "options":{
+            "a":"Robert Clark",
+            "b":"Rob Pike",
+            "c":"Ken Thomson",
+            "d":"Robert Greisemer"
+        },
+        "answer":"a",
+        "explanation":"fact"
+    },
+    {
+        "id":"2",
+        "question":"Which is not included in the hello world program created at the first part of the session?",
+        "options":{
+            "a":"import fmt",
+            "b":"import fnt",
+            "c":"package main",
+            "d":"func main()"
+        },
+        "answer":"b",
+        "explanation":"fnt is not a package"
+    }
+]
 let port = process.env.port || 5000;
 
-
 const express = require("express");
-// const mongoose  = require('mongoose');
-// const { findOne } = require("domutils");
+const mongoose = require("mongoose");
+const { findOne } = require("domutils");
 
-// mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 
-// try {
-//     const connect = mongoose.connect('mongodb://localhost:27017/golang',)
-//     console.log("db working");
-    
-// } catch (error) {
-//     console.log(error);
-// // }
-// const User = mongoose.model('User', { name: String, score: Number });
-
-
+try {
+  const connect = mongoose.connect("mongodb://localhost:27017/golang");
+  console.log("db working");
+} catch (error) {
+  console.log(error);
+}
+const User = mongoose.model("User", {
+  name:{ type:String,
+        required:true},
+  score: Number,
+});
 
 const app = express();
 app.use(express.json());
 
-
-app.get("/" , (req,res) => {
-    res.send("Hello world");
+app.get("/", (req, res) => {
+  res.send("Hello world");
 });
 
-app.get("/questions", (req,res) => {
-    res.send(importData);
+app.get("/questions", (req, res) => {
+  res.send(importData);
+});
+
+app.post("/user", async (req, res) => {
+  const { name, score } = req.body;
+  
+  try {
+    const newUser = new User({
+      name: name,
+      score: score,
+    });
+    await newUser.save();
+    console.log({ _id: newUser.id });
+    res.status(200).json({ _id: newUser.id });
+    return;
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+//add score
+app.put("/updateUser", async (req, res) => {
+  try {
+    const { _id, score } = req.body;
+    if(mongoose.Types.ObjectId.isValid(_id))
+    {
+      const updateUser = await User.findByIdAndUpdate(_id, { score: score });
+      res.status(200).json(updateUser);
+      return;
+    }
+    else
+    {
+      res.send("not a valid id. Contact WLUGian for scores")
+        return ; 
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(401).json(error);
+  }
 });
 
 
-// app.post("/user",async(req,res)=>{
-//     const {name, score} = req.body;
-//     try {
-//         const newUser  = new User({
-    
-//             name:name,
-//             score:score
-//         })
-//         await newUser.save();
-//         console.log({_id:newUser.id});
-//         res.status(200).json({_id : newUser.id});
-//         return
-//     } catch (error) {
-//         res.status(400).send(error);
-//     }
-// });
-// //add score
-// app.put('/updateUser',async(req,res)=>{
-//     try {
-//         const {userId, score} =req.body;
-//         console.log(userId);
-//         const updateUser = await User.findByIdAndUpdate(userId,{score:score});
-//         res.status(200).json(updateUser);
-//         return
-        
-//     } catch (error) {
-//         console.log(error)
-//         res.status(401).json(error)
-//     }
-// })
-
-
-// app.get('/data',(req,res)=>{
-
-// })
 app.listen(port, () => {
-    console.log(`Eg app is listening on port http://localhost:3000`);
-})
+  console.log(`Eg app is listening on port http://localhost:5000`);
+});
--- main.go
+++ main.go
@@ -1,47 +1,50 @@
 package main
 
 import (
-	//"bytes"
+	"bytes"
 	"encoding/json"
 	"fmt"
-	//"net/http"
+	"io/ioutil"
+	"net/http"
+	"strconv"
 	"time"
 )
 
-
 type Person struct {
-    name string `json:"name"`
-    score  int    `json:"score"`
+	name  string
+	score string
 }
 
-
 func main() {
-	url := "http://localhost:3000/questions"
-
+	url := "http://localhost:5000/questions"
 	var name string
 	fmt.Print("Enter your name : ")
-	fmt.Scanf("%s",&name);
+	nameC := make(chan string)
+	go func() {
+		fmt.Scanf("%s", &name)
+		nameC <- name
+	}()
 
+	nam := <-nameC
+	m := map[string]string{
+		"name": nam,
+	}
+	jsonData, err := json.Marshal(m)
+	if err != nil {
+		fmt.Print("conversion to json went wrong ")
+		return
+	}
+	postData, err := http.Post("http://localhost:5000/user", "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Print("Something went wrong in post req")
+	}
+	id, _ := ioutil.ReadAll(postData.Body)
+	fmt.Print(string(id))
 
-	data := Person{name: name}
-    jsonData, _ := json.Marshal(data)
-
-	fmt.Print(data); 
-	fmt.Print(jsonData); 
-
-
-	// _, err := http.Post("http://localhost:3000/user", "application/json", bytes.NewBuffer(jsonData));
-	// if err!=nil{
-	// 	fmt.Print("Something went wrong");
-	// }
-
-
-	
 	problems := questionPuller(url)
+	tobj := time.NewTimer(20 * time.Second) // Time for all the questions --> 1 question => 10 seconds
 
-	tobj := time.NewTimer(20*time.Duration(len(problems)) * time.Second) // Time for all the questions --> 1 question => 10 seconds
-
-	correctAns := 0
+	var correctAns int = 0
 ProblemLoop:
 
 	for i, problem := range problems {
@@ -55,24 +58,64 @@ ProblemLoop:
 			fmt.Scanf("%s", &answer)
 			ansC <- answer
 		}()
-		
-		
+
 		select {
 		case <-tobj.C:
-			fmt.Println("\nTime Over !!! Your Quiz has been Submitted")
+			fmt.Println("\nTime Over !!! Your Quiz has been Submitted\n")
 			break ProblemLoop
 		case iAns := <-ansC:
 			if iAns == problem.Answer {
 				correctAns++
 			}
 			if i == len(problems)-1 {
-				fmt.Print("All Questions Submitted Successfully...:) ")
+				fmt.Print("All Questions Submitted Successfully...:)\n ")
 			}
 
 		}
 
 	}
 
+	var scnid string
+
+	fmt.Println("Enter You id")
+	fmt.Scan(&scnid)
+
+	m2 := map[string]string{
+		"_id":   scnid,
+		"score": strconv.Itoa(correctAns),
+	}
+	jsonData2, err := json.Marshal(m2)
+	if err != nil {
+		fmt.Print("conversion to json went wrong ")
+		return
+	}
+	//postData2, err := http.put("http://localhost:5000/user", "application/json", bytes.NewBuffer(jsonData2));
+	// postData2, err := http.NewRequest("PUT", "http://localhost:5000/updateUser", bytes.NewBuffer(jsonData2))
+	// if err != nil {
+	// 	fmt.Print("Something went wrong in post req")
+	// }
+	// id2, _ := ioutil.ReadAll(postData2.Body)
+	// fmt.Print(string(id2))
+
+
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodPut, "http://localhost:5000/updateUser", bytes.NewBuffer(jsonData2))
+    if err != nil {
+        panic(err)
+    }
+
+    // set the request header Content-Type for json
+    req.Header.Set("Content-Type", "application/json; charset=utf-8")
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+
+	id2, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(id2))
 	fmt.Printf("Correct %d out of %d", correctAns, len(problems))
 
+
 }
+
+
